f98b6520b2878e447236510196d7c1ff9b4016c6
Srikanth Marrapu <srikanth23111995@gmail.com>
2021-09-20 02:47:01 +0530

    Fetching data from firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,7 +8,7 @@ import CartContext from '../../store/cart-context';
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
 
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const totalAmount = `₹${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,38 @@
 import Card from '../UI/Card';
 import MealItem from './MealItems/MealItems';
 import classes from './AvailableMeals.module.css';
+import {useEffect,useState} from 'react';
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-  },
-];
+
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals,setMeals]=useState([]);
+
+useEffect(()=>{
+  const fetchMeals=async () => {
+    const response=await fetch('https://react-http-71a5f-default-rtdb.firebaseio.com/meals.json');
+    const responseData=await response.json();
+    const loadedMeal=[]
+    for(const key in responseData){
+      loadedMeal.push({
+        id:key,
+        name:responseData[key].name,
+        description:responseData[key].description,
+        price:responseData[key].price
+      })
+      
+    }
+    
+    setMeals(loadedMeal);
+    
+    
+  }
+  fetchMeals();
+},[])
+console.log(meals);
+  
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       key={meal.id}
       id={meal.id}
